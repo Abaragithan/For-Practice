d14ac834acf1528d6484c987833bc899c235324a
Abaragithan <abaragithan02@gmail.com>
2024-06-12 17:12:41 +0530

    LeetCode problem

---
+++ LeetCode/MissingNumber.java
@@ -0,0 +1,28 @@
+package Practice.LeetCode;
+
+public class MissingNumber {
+    public static void main(String[] args) {
+        int[] nums = {0,1,3};
+        System.out.println(missing(nums));
+    }
+
+    private static int missing(int[] nums) {
+        int i = 0;
+        while (i < nums.length){
+            int index = nums[i];
+            if (nums[i] < nums.length && nums[i] != nums[index]){
+                int temp = nums[i];
+                nums[i] = nums[index];
+                nums[index] = temp;
+            } else {
+                i++;
+            }
+        }
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j){
+                return j;
+            }
+        }
+        return nums.length;
+    }
+}
